5234121a434ad4d358ea444ab43700da3c6cc449
ZhangRay30 <rayzhang30@163.com>
2023-02-19 17:13:09 +0800

    create the README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+maven demo for csc1004
+
+date is 2023-2-19
--- src/main/java/hellomaven.java
+++ src/main/java/hellomaven.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class hellomaven {
+public class hellomaven {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
 }
